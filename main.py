8e129857879becbbc60ebf8c2d2e041e99b13e70
Mishamvt1976 <mishamvt@gmail.com>
2026-06-04 21:56:33 +0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+#first_name = "misha"
+
+#print("first_name")
+#print("Hello world")
+#print ("Hello", "first_neme")
+
+# last_name = "vardosshvili"
+# age = 50
+
+# print(id(last_name))
+# print(id(age))
+
+# first_name = "Misha"
+# last_name = "Vardoshvili"
+# age = 50
+# float_variable = 50.5
+# student = False
+# teacher = True
+# none_variable = None
+
+# a = int(True)
+# a = int(False)
+# print(a)
