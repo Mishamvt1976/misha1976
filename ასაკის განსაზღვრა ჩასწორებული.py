277db70830411fea0768a16aedcfd6260dcedbf6
Mishamvt1976 <mishamvt@gmail.com>
2026-06-11 22:02:16 +0400

    Add files via upload

---
+++ ასაკის განსაზღვრა ჩასწორებული.py
@@ -0,0 +1,23 @@
+try:
+
+    age_input = input("შეიყვანოთ თქვენი ასაკი: ")
+
+    age = int (age_input)
+
+    if age < 0:
+
+       raise ValueError ("ასაკი არ შეიძლება იყოს უარყოფითი")
+
+    if age < 18:
+
+         print("თქვენ ხართ არასრულწლოვანი.")
+    else:
+         print("თქვენ ხართ სრულწლოვანი.")
+   
+except ValueError as e:
+   
+    if not age_input.isdigit():
+            print("შეიყვანეთ მხოლოდ რიცხვი!")
+    else:
+        
+        print(f"შეცდომა: {e}")
